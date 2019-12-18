6480775fb1e036715c822d575f7eb40c19d07ee5
Nikske <wolfvdd@gmail.com>
2019-12-18 10:02:33 +0100

    completed 3-3

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -12,5 +12,13 @@
 (function() {
 
     // your code here
-
+    document.getElementById("run").addEventListener("click", function() {
+        // Got the solution thanks to this answer on stackOverflow written by a lizard: https://stackoverflow.com/a/5092872
+        // Creates a variable first populated with a black background colour (#000000), replacing the 0s randomly with one of the 16 hexadecimal symbols, converting them to a string so they can be used in the background color.
+        let randomColor = "#000000".replace(/0/g, function () {
+            return (~~(Math.random() * 16)).toString(16);
+        });
+        document.body.style.backgroundColor = randomColor;
+        console.log(randomColor);
+    });
 })();
