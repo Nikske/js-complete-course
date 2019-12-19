187f7ecd0e2d5bc94fca677f36f3c3163d88f8bb
Nikske <wolfvdd@gmail.com>
2019-12-19 09:16:36 +0100

    4-1 finished

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -14,5 +14,12 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let getDate = new Date();
+    let hours = getDate.getHours();
+    if (hours <=18) {
+        document.getElementById("target").innerHTML = "Hello"
+    } else {
+        document.getElementById("target").innerHTML = "Good evening"
+    }
 
 })();
