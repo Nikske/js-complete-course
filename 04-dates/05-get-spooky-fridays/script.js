be7c77ffc15d70d093a437735cd420c2b21d05e1
Nikske <wolfvdd@gmail.com>
2019-12-20 09:53:30 +0100

    Finished 4-5 before I continue, mostly thanks to Steven's help

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -12,5 +12,22 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function() {
+        let userYear = Number(document.getElementById("year").value);
+        function month13(year) {
+            // Going through each month until we run out
+            for (let month = 0; month < 12; month++) {
+                // Check for every 13th day
+                let date = new Date(year, month, 13);
+                // Check if it's a friday
+                if (date.getDay() === 5) {
+                    const monthArray = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
+                    alert(monthArray[Number(month)]);
+                }
+            }
+        }
+        console.log(month13(userYear));
+    });
 
 })();
