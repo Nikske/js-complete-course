f5fb2efc0621df716886a2b149a5a280869a128d
Nikske <wolfvdd@gmail.com>
2019-12-20 11:48:25 +0100

    part 1 of 5-9 done, created a random array and I can place them into the HTMl smoothly

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -14,6 +14,11 @@
     // your code here
     document.getElementById("run").addEventListener("click", function() {
         let randomArray = Array(10).fill().map(() => Math.round(Math.random() * 100));
+        let i = 1;
+        for (i; i <= 10; i++) {
+            let tableID = ("n-" + i);
+            document.getElementById(tableID).innerHTML = randomArray[i - 1];
+        }
         console.log(randomArray);
     });
 })();
