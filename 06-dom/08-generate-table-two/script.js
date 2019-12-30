92f1044b6b826065fc9a880430cdaff4928eaefb
Nikske <wolfvdd@gmail.com>
2019-12-30 11:33:04 +0100

    finished 6-8

--- 06-dom/08-generate-table-two/script.js
+++ 06-dom/08-generate-table-two/script.js
@@ -12,5 +12,17 @@
 (function() {
 
     // your code here
+    // Create table and place it in the #target div
+    let table = document.createElement("table");
+    document.getElementById("target").appendChild(table);
 
+    // 10 rows, start at 1
+    for(let rows = 1; rows <= 10; rows++){
+        let row = table.insertRow();
+        // 10 columns
+        for(let cols = 1; cols <= 10; cols++) {
+            let col = row.insertCell();
+            col.innerHTML= rows * cols;
+        }
+    }
 })();
